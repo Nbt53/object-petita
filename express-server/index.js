77c2a6c1c476e14de6f4287e8abcceea67087e1a
nbt53 <nbt853.nh@gmail.com>
2024-03-08 08:08:43 +0000

    debugging render

--- express-server/index.js
+++ express-server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve Vite-built files as static
-app.use(express.static(path.join(__dirname, '../front-end/dist'), { index: 'index.html' }));
+app.use(express.static(path.join(__dirname, '../front-end/dist')));
 
 
 app.get('/files', (req, res) => {
