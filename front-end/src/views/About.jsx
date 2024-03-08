9d65410acdac51c4a08c21b97ecea929fcb19d6c
nbt53 <nbt853.nh@gmail.com>
2024-03-08 07:50:40 +0000

    debugging

--- front-end/src/views/About.jsx
+++ front-end/src/views/About.jsx
@@ -5,6 +5,41 @@ export default function About() {
     return (
         <section className="screen-container">
             <ViewTitle title="About the artist" />
+            <div className="about-container">
+                <img src="./images/about.png" alt="" className="about-img" />
+
+                <div className="about-text-container">
+                    <p className="about-text mb-small">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi non
+                        laudantium cumque enim. Quibusdam nisi enim consequuntur perspiciatis obcaecati minima,
+                        hic libero ab praesentium sunt, illum id eum nihil facilis!    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi non
+                        laudantium cumque enim. Quibusdam nisi enim consequuntur perspiciatis obcaecati minima,
+                        hic libero ab praesentium sunt, illum id eum nihil facilis!
+                    </p>
+                    <p className="about-text mb-small">
+                        Blanditiis quod optio dolore modi ea molestiae quae dicta consectetur eum
+                        corrupti dolorem aliquid, nisi sed magni voluptatum sapiente illo maxime reprehenderit.
+                        Aspernatur fugiat a at, animi possimus tenetur repudiandae?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi non
+                        laudantium cumque enim. Quibusdam nisi enim consequuntur perspiciatis obcaecati minima,
+                        hic libero ab praesentium sunt, illum id eum nihil facilis!
+                    </p>
+                    <p className="about-text">
+                        At eius voluptate est, sunt deserunt corporis? Dicta cumque enim sapiente illo autem
+                        repudiandae. Obcaecati, repellat voluptates maiores ab quaerat mollitia ipsa, aliquam hic,
+                        corrupti dolorum rem id delectus tempore.
+                    </p>
+
+                    <ul className="about-list">
+                        <li className="about-list-item mb-small">Big Bird Art fair 2022</li>
+                        <li className="about-list-item mb-small">Beakfest 2023</li>
+                        <li className="about-list-item mb-small">Dark Pottery 2023</li>
+                        <li className="about-list-item mb-small">Ceribal Ceramics 2024</li>
+                    </ul>
+
+                </div>
+            </div>
+
+
         </section>
     )
 }
--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,4 +1,3 @@
-import Slideshow from "../components/Slideshow";
 import ViewTitle from "../components/ViewTitle";
 
 export default function Home() {
@@ -6,29 +5,36 @@ export default function Home() {
     return (
         <section className="screen-container">
             <ViewTitle title={'Big Art Girl'} />
+            <div className="home">
+                <div className="home-video__container">
+                    <video width="360" height="600" className="home-video" autoPlay loop muted>
+                        <source src="./images/videos/mother-video.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
 
-            <Slideshow />
-            <div className="home-text-container">
-                <p className="home-text mb-medium">
-                    Welcome to object petita Ceramic Studio, where art meets introspection in a unique exploration of
-                    the human experience. Delve into the captivating world of ceramics that transcends traditional
-                    boundaries. My creations are a reflection of the profound impact figures in our lives can have
-                    on our mental well-being. Through the artful manipulation of deformed human figures,
-                    I aim to evoke a visceral response and encourage contemplation on the complexities of
-                    our emotions.</p>
-                <p className="home-text mb-medium">
-                    Each piece tells a story, portraying the delicate dance between the external world and our inner
-                    thoughts. The transformative power of my work lies in the deliberate distortion of figures,
-                    set against diverse and evocative backdrops. These settings serve as a visual metaphor for
-                    the myriad influences that shape our mental health. Whether it is the struggle for identity,
-                    the impact of societal expectations, or the harmony found in embracing imperfections
-                    , my ceramics provide a tangible medium to explore the nuances of the human psyche.</p>
-                <p className="home-text mb-medium">
-                    Explore the gallery to witness a fusion of art and psychology, where distorted forms spark
-                    conversations about resilience, vulnerability, and the beauty that arises from embracing
-                    the complexities of our minds. Join me on this introspective journey through clay,
-                    as we unravel the intricate threads of human experience and celebrate the resilience
-                    found in the intersection of art and mental health.</p>
+                <div className="home-text-container">
+                    <p className="home-text mb-medium">
+                        Welcome to object petita Ceramic Studio, where art meets introspection in a unique exploration of
+                        the human experience. Delve into the captivating world of ceramics that transcends traditional
+                        boundaries. My creations are a reflection of the profound impact figures in our lives can have
+                        on our mental well-being. Through the artful manipulation of deformed human figures,
+                        I aim to evoke a visceral response and encourage contemplation on the complexities of
+                        our emotions.</p>
+                    <p className="home-text mb-medium">
+                        Each piece tells a story, portraying the delicate dance between the external world and our inner
+                        thoughts. The transformative power of my work lies in the deliberate distortion of figures,
+                        set against diverse and evocative backdrops. These settings serve as a visual metaphor for
+                        the myriad influences that shape our mental health. Whether it is the struggle for identity,
+                        the impact of societal expectations, or the harmony found in embracing imperfections
+                        , my ceramics provide a tangible medium to explore the nuances of the human psyche.</p>
+                    <p className="home-text mb-medium">
+                        Explore the gallery to witness a fusion of art and psychology, where distorted forms spark
+                        conversations about resilience, vulnerability, and the beauty that arises from embracing
+                        the complexities of our minds. Join me on this introspective journey through clay,
+                        as we unravel the intricate threads of human experience and celebrate the resilience
+                        found in the intersection of art and mental health.</p>
+                </div>
             </div>
         </section>
     )
